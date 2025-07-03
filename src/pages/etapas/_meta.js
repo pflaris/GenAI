d3e044adea1ce08e28b1217aec0ae35cebcebd7e
Larissa <149023757+pflaris@users.noreply.github.com>
2025-07-03 11:05:47 -0300

    Create _meta.js

---
+++ src/pages/etapas/_meta.js
@@ -0,0 +1,13 @@
+export default {
+    "etapa1": "Tema e delimitação",
+    "etapa2": "Problematização",
+    "etapa3": "Elaboração do objetivo",
+    "etapa4": "Redação da introdução",
+    "etapa5": "Competência em informação",
+    "etapa11": "Instrumentos de pesquisa",
+    "etapa12": "Materiais e procedimentos metodológicos",
+    "etapa13": "Resultados parciais",
+    "etapa14": "Resumo, abstract e título",
+    "etapa15": "Redação do Projeto de Pesquisa",
+    "etapa16": "Defesa oral"
+}
